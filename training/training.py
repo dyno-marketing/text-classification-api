a0406d7e4365acf70e40e57821cfdf47a9ec863a
Tu Pham <phamptu@gmail.com>
2016-10-30 15:51:25 +0700

    refactor(training)

--- training/training.py
+++ training/training.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
 from lxml import objectify
+
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.externals import joblib
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cross_validation import train_test_split
+
+from handler import ItemSelector
 
 
 def parse_xml(path):
-    xml = objectify.parse(open(path))
+    print("path ", path)
+    xml = objectify.parse(open(path, 'r', encoding='utf-8'))
     root = xml.getroot()
     children = root.getchildren()
     df = pd.DataFrame(columns=('id', 'name', 'category', 'about', 'description', 'fans_country_VN'))
@@ -18,87 +35,45 @@ def parse_xml(path):
     return df
 
 
-page_accounts = parse_xml("page_accountant.xml")
-page_chinese = parse_xml("page_chinese.xml")
-page_english = parse_xml("page_english.xml")
-page_restaurant = parse_xml("page_restaurant.xml")
-page_travel = parse_xml("page_travel.xml")
-other = parse_xml("page_drop.xml")
+gnb = GaussianNB()
 
-page_accounts["class"] = "accounts"
-page_chinese["class"] = "chinese"
-page_english["class"] = "english"
-page_restaurant["class"] = "restaurant"
-page_travel["class"] = "travel"
-other["class"] = "other"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("dir_path ", dir_path)
+train_data_dir_path = dir_path + "/data/train/"
+print("train_data_dir_path ", train_data_dir_path)
 
-data = pd.concat([page_accounts, page_chinese, page_english, page_restaurant, page_travel, other])
+
+def load_data():
+    global page_accountant, page_chinese, page_english, page_hotel, page_restaurant, page_travel, page_other, page_vocational
+    page_accountant = parse_xml(train_data_dir_path + "accountant/page_accountant.xml")
+    page_chinese = parse_xml(train_data_dir_path + "chinese/page_chinese.xml")
+    page_english = parse_xml(train_data_dir_path + "english/page_english.xml")
+    page_hotel = parse_xml(train_data_dir_path + "hotel/page_hotel.xml")
+    page_it = parse_xml(train_data_dir_path + "it/page_it.xml")
+    page_other = parse_xml(train_data_dir_path + "other/page_other.xml")
+    page_restaurant = parse_xml(train_data_dir_path + "restaurant/page_restaurant.xml")
+    page_travel = parse_xml(train_data_dir_path + "travel/page_travel.xml")
+    page_vocational = parse_xml(train_data_dir_path + "vocational/page_vocational.xml")
+
+    page_accountant["class"] = "accountant"
+    page_chinese["class"] = "chinese"
+    page_english["class"] = "english"
+    page_hotel["class"] = "hotel"
+    page_it["class"] = "it"
+    page_other["class"] = "other"
+    page_restaurant["class"] = "restaurant"
+    page_travel["class"] = "travel"
+    page_vocational["class"] = "vocational"
+
+
+load_data()
+
+data = pd.concat([page_accountant, page_chinese, page_english, page_restaurant, page_travel, page_other, page_hotel,
+                  page_vocational])
 
 data.fillna(value="", inplace=True)
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cross_validation import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(
-    data, data["class"], test_size=0.33, random_state=42)
-
-from sklearn.pipeline import Pipeline, FeatureUnion
-
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.datasets.twenty_newsgroups import strip_newsgroup_footer
-from sklearn.datasets.twenty_newsgroups import strip_newsgroup_quoting
-from sklearn.decomposition import TruncatedSVD
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import classification_report
-from sklearn.pipeline import FeatureUnion
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
-
-
-class ItemSelector(BaseEstimator, TransformerMixin):
-    """For data grouped by feature, select subset of data at a provided key.
-
-    The data is expected to be stored in a 2D data structure, where the first
-    index is over features and the second is over samples.  i.e.
-
-    >> len(data[key]) == n_samples
-
-    Please note that this is the opposite convention to scikit-learn feature
-    matrixes (where the first index corresponds to sample).
-
-    ItemSelector only requires that the collection implement getitem
-    (data[key]).  Examples include: a dict of lists, 2D numpy array, Pandas
-    DataFrame, numpy record array, etc.
-
-    >> data = {'a': [1, 5, 2, 5, 2, 8],
-               'b': [9, 4, 1, 4, 1, 3]}
-    >> ds = ItemSelector(key='a')
-    >> data['a'] == ds.transform(data)
-
-    ItemSelector is not designed to handle data grouped by sample.  (e.g. a
-    list of dicts).  If your data is structured this way, consider a
-    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
-
-    Parameters
-    ----------
-    key : hashable, required
-        The key corresponding to the desired value in a mappable.
-    """
-
-    def __init__(self, key):
-        self.key = key
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, data_dict):
-        return data_dict[self.key]
-
-
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
+x_train, x_test, y_train, y_test = train_test_split(data, data["class"], test_size=0.33, random_state=42)
 
 combined_features = Pipeline([("feature", FeatureUnion(
     transformer_list=[
@@ -107,7 +82,6 @@ combined_features = Pipeline([("feature", FeatureUnion(
             ('selector', ItemSelector(key='name')),
             ('tfidf', TfidfVectorizer(min_df=50)),
         ])),
-
         # Pipeline for standard bag-of-words model for body
         ('description', Pipeline([
             ('selector', ItemSelector(key='description')),
@@ -119,27 +93,17 @@ combined_features = Pipeline([("feature", FeatureUnion(
         ]))
     ])), ('chi', SelectKBest(score_func=chi2, k=50))])
 
-combined_features.fit(X_train, y_train)
+combined_features.fit(x_train, y_train)
+x_fommated = combined_features.transform(x_train)
 
-X_fommated = combined_features.transform(X_train)
+model = gnb.fit(x_fommated.toarray(), y_train)
+print('LogisticRegression score: %f' % model.score(combined_features.transform(x_test).toarray(), y_test))
 
-from sklearn.naive_bayes import GaussianNB
+confusion_matrix(y_test, model.predict(combined_features.transform(x_test).toarray()))
+print(classification_report(y_test, model.predict(combined_features.transform(x_test).toarray())))
 
-gnb = GaussianNB()
-model = gnb.fit(X_fommated.toarray(), y_train)
-
-print('LogisticRegression score: %f'
-      % model.score(combined_features.transform(X_test).toarray(), y_test))
-
-from sklearn.metrics import confusion_matrix
-
-confusion_matrix(y_test, model.predict(combined_features.transform(X_test).toarray()))
-
-from sklearn.metrics import classification_report
-
-print(classification_report(y_test, model.predict(combined_features.transform(X_test).toarray())))
-
-from sklearn.externals import joblib
-
-joblib.dump(combined_features, 'tfidf_model.pkl')
-joblib.dump(model, 'prediction_model.pkl')
+dir_path = dir_path.replace("/training", "")
+model_data_dir_path = dir_path + "/model/"
+print("model_data_dir_path ", model_data_dir_path)
+joblib.dump(combined_features, model_data_dir_path + 'tfidf_model.pkl')
+joblib.dump(model, model_data_dir_path + 'prediction_model.pkl')
